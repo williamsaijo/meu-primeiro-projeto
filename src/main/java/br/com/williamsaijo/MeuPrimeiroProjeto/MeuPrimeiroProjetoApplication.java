b7a69933f572358f7ef3164dd76e0cfead677cc5
William Saijo Nakashima Konishi1 <william.saijo1@ibm.com>
2022-02-18 15:20:40 -0300

    Versão inicial do projeto

---
+++ src/main/java/br/com/williamsaijo/MeuPrimeiroProjeto/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.com.williamsaijo.MeuPrimeiroProjeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/williamsaijo/MeuPrimeiroProjeto/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.williamsaijo.MeuPrimeiroProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
